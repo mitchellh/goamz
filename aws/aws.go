5514945fdecc0e20ea0e782624a50cefec677cfd
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-07-29 17:10:53 -0700

    aws: timeout on requesting metadata

--- aws/aws.go
+++ aws/aws.go
@@ -14,8 +14,10 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 // Region defines the URLs where AWS services may be accessed.
@@ -175,9 +177,23 @@ type credentials struct {
 }
 
 func GetMetaData(path string) (contents []byte, err error) {
+	c := http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				deadline := time.Now().Add(5 * time.Second)
+				c, err := net.DialTimeout(netw, addr, time.Second*2)
+				if err != nil {
+					return nil, err
+				}
+				c.SetDeadline(deadline)
+				return c, nil
+			},
+		},
+	}
+
 	url := "http://169.254.169.254/latest/meta-data/" + path
 
-	resp, err := http.Get(url)
+	resp, err := c.Get(url)
 	if err != nil {
 		return
 	}
