0cf779e8fad7e39937dccb250a9bc75c1b910c6a
Matthew Hooker <mwhooker@gmail.com>
2013-11-01 22:08:41 -0700

    aws retry predicate method

--- aws/client.go
+++ aws/client.go
@@ -2,15 +2,36 @@ package aws
 
 import (
 	"github.com/mwhooker/go.httpcontrol"
+	"net"
 	"net/http"
 	"time"
 )
+
+func awsRetry(req *http.Request, res *http.Response, err error) bool {
+	retry := false
+
+	if err == nil && res != nil {
+		retry = false
+	}
+	if neterr, ok := err.(net.Error); ok {
+		if neterr.Temporary() {
+			retry = true
+		}
+	}
+	if res != nil {
+		if 500 > res.StatusCode && res.StatusCode >= 400 {
+			retry = true
+		}
+	}
+	return retry
+}
 
 var RetryingClient = &http.Client{
 	Transport: &httpcontrol.Transport{
 		Proxy:          http.ProxyFromEnvironment,
 		RequestTimeout: time.Second * 5,
 		DialTimeout:    time.Second * 2,
-		MaxTries:       3,
+		ShouldRetry:    awsRetry,
+		Wait:           httpcontrol.Wait(httpcontrol.ExpBackoff),
 	},
 }
