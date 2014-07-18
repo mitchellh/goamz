70fcf29109aa17abed5b094fc84954789cddbadb
Alan Shreve <alan@inconshreveable.com>
2014-07-18 11:37:30 -0700

    add head method

--- s3/s3.go
+++ s3/s3.go
@@ -210,6 +210,29 @@ func (b *Bucket) GetResponse(path string) (*http.Response, error) {
 	panic("unreachable")
 }
 
+func (b *Bucket) Head(path string) (*http.Response, error) {
+	req := &request{
+		method: "HEAD",
+		bucket: b.Name,
+		path:   path,
+	}
+	err := b.S3.prepare(req)
+	if err != nil {
+		return nil, err
+	}
+	for attempt := attempts.Start(); attempt.Next(); {
+		resp, err := b.S3.run(req, nil)
+		if shouldRetry(err) && attempt.HasNext() {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
+		return resp, nil
+	}
+	panic("unreachable")
+}
+
 // Put inserts an object into the S3 bucket.
 //
 // See http://goo.gl/FEBPD for details.
