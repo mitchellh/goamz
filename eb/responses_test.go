431ae3b1bee1b7a04a6143b9978e03abc8c2064a
Sebastian Cole <sebastian.paris@gmail.com>
2014-10-26 16:54:51 +1100

    Initial structure for ElasticBeanstalk
    Added EBEndpoint to aws.go
    Cloned lots of util functions from elb package
    implemented CreateApplication for eb

---
+++ eb/responses_test.go
@@ -0,0 +1,22 @@
+package eb_test
+
+// http://docs.aws.amazon.com/elasticbeanstalk/latest/api/API_CreateApplication.html
+var CreateApplicationExample = `
+<CreateApplicationResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
+  <CreateApplicationResult>
+    <Application>
+      <Versions/>
+      <Description>Sample Description</Description>
+      <ApplicationName>SampleApp</ApplicationName>
+      <DateCreated>2010-11-16T23:09:20.256Z</DateCreated>
+      <DateUpdated>2010-11-16T23:09:20.256Z</DateUpdated>
+      <ConfigurationTemplates>
+        <member>Default</member>
+      </ConfigurationTemplates>
+    </Application>
+  </CreateApplicationResult>
+  <ResponseMetadata>
+    <RequestId>8b00e053-f1d6-11df-8a78-9f77047e0d0c</RequestId>
+  </ResponseMetadata>
+</CreateApplicationResponse> 
+`
