96429f1120cfe778e1749d306112d94e62607884
sebastian cole <sebastian.cole@mi9.com.au>
2014-10-27 15:05:01 +1100

    Added CreateApplicationVersion method

--- eb/eb_test.go
+++ eb/eb_test.go
@@ -86,3 +86,38 @@ func (s *S) TestCheckDNSAvailability(c *C) {
 	c.Assert(resp.Available, Equals, true)
 	c.Assert(resp.RequestId, Equals, "12f6701f-f1d6-11df-8a78-9f77047e0d0c")
 }
+
+func (s *S) TestCreateApplicationVersion(c *C) {
+	testServer.Response(200, nil, CreateApplicationVersionExample)
+
+	options := eb.CreateApplicationVersion{
+		ApplicationName:       "SampleApp",
+		VersionLabel:          "Version1",
+		Description:           "description",
+		AutoCreateApplication: true,
+		SourceBundle: eb.S3Location{
+			S3Bucket: "amazonaws.com",
+			S3Key:    "sample.war",
+		},
+	}
+
+	resp, err := s.eb.CreateApplicationVersion(&options)
+	req := testServer.WaitRequest()
+
+	c.Assert(req.Form["Action"], DeepEquals, []string{"CreateApplicationVersion"})
+	c.Assert(req.Form["ApplicationName"], DeepEquals, []string{"SampleApp"})
+	c.Assert(req.Form["VersionLabel"], DeepEquals, []string{"Version1"})
+	c.Assert(req.Form["Description"], DeepEquals, []string{"description"})
+	c.Assert(req.Form["AutoCreateApplication"], DeepEquals, []string{"true"})
+	c.Assert(req.Form["SourceBundle.S3Bucket"], DeepEquals, []string{"amazonaws.com"})
+	c.Assert(req.Form["SourceBundle.S3Key"], DeepEquals, []string{"sample.war"})
+	c.Assert(err, IsNil)
+	c.Assert(resp.ApplicationName, Equals, "SampleApp")
+	c.Assert(resp.DateCreated, Equals, "2010-11-17T03:21:59.161Z")
+	c.Assert(resp.DateUpdated, Equals, "2010-11-17T03:21:59.161Z")
+	c.Assert(resp.Description, Equals, "description")
+	c.Assert(resp.SourceBundle.S3Bucket, Equals, "amazonaws.com")
+	c.Assert(resp.SourceBundle.S3Key, Equals, "sample.war")
+	c.Assert(resp.VersionLabel, Equals, "Version1")
+	c.Assert(resp.RequestId, Equals, "d653efef-f1f9-11df-8a78-9f77047e0d0c")
+}
--- eb/responses_test.go
+++ eb/responses_test.go
@@ -1,5 +1,18 @@
 package eb_test
 
+// http://docs.aws.amazon.com/elasticbeanstalk/latest/api/API_CheckDNSAvailability.html
+var CheckDNSAvailabilityExample = `
+ <CheckDNSAvailabilityResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
+  <CheckDNSAvailabilityResult>
+    <FullyQualifiedCNAME>sampleapplication.elasticbeanstalk.amazonaws.com</FullyQualifiedCNAME>
+    <Available>true</Available>
+  </CheckDNSAvailabilityResult>
+  <ResponseMetadata>
+    <RequestId>12f6701f-f1d6-11df-8a78-9f77047e0d0c</RequestId>
+  </ResponseMetadata>
+</CheckDNSAvailabilityResponse>
+`
+
 // http://docs.aws.amazon.com/elasticbeanstalk/latest/api/API_CreateApplication.html
 var CreateApplicationExample = `
 <CreateApplicationResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
@@ -21,21 +34,33 @@ var CreateApplicationExample = `
 </CreateApplicationResponse> 
 `
 
+// http://docs.aws.amazon.com/elasticbeanstalk/latest/api/API_CreateApplicationVersion.html
+var CreateApplicationVersionExample = `
+<CreateApplicationVersionResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
+  <CreateApplicationVersionResult>
+    <ApplicationVersion>
+      <SourceBundle>
+        <S3Bucket>amazonaws.com</S3Bucket>
+        <S3Key>sample.war</S3Key>
+      </SourceBundle>
+      <VersionLabel>Version1</VersionLabel>
+      <Description>description</Description>
+      <ApplicationName>SampleApp</ApplicationName>
+      <DateCreated>2010-11-17T03:21:59.161Z</DateCreated>
+      <DateUpdated>2010-11-17T03:21:59.161Z</DateUpdated>
+    </ApplicationVersion>
+  </CreateApplicationVersionResult>
+  <ResponseMetadata>
+    <RequestId>d653efef-f1f9-11df-8a78-9f77047e0d0c</RequestId>
+  </ResponseMetadata>
+</CreateApplicationVersionResponse> 
+`
+
+// http://docs.aws.amazon.com/elasticbeanstalk/latest/api/API_DeleteApplication.html
 var DeleteApplicationExample = `
 <DeleteApplicationResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
   <ResponseMetadata>
     <RequestId>1f155abd-f1d7-11df-8a78-9f77047e0d0c</RequestId>
   </ResponseMetadata>
 </DeleteApplicationResponse>
-`
-var CheckDNSAvailabilityExample = `
- <CheckDNSAvailabilityResponse xmlns="https://elasticbeanstalk.amazonaws.com/docs/2010-12-01/">
-  <CheckDNSAvailabilityResult>
-    <FullyQualifiedCNAME>sampleapplication.elasticbeanstalk.amazonaws.com</FullyQualifiedCNAME>
-    <Available>true</Available>
-  </CheckDNSAvailabilityResult>
-  <ResponseMetadata>
-    <RequestId>12f6701f-f1d6-11df-8a78-9f77047e0d0c</RequestId>
-  </ResponseMetadata>
-</CheckDNSAvailabilityResponse>
 `
