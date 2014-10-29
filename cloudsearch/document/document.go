9f0cd9acce0e179df5f5cf03dd166fc238f92b26
Niels Draaisma <niels.draaisma@producthealth.com>
2014-10-29 17:03:19 +0000

    Code cleanup

--- cloudsearch/document/document.go
+++ cloudsearch/document/document.go
@@ -1,20 +1,20 @@
 package document
 
 import (
-	"net/http"
-	"github.com/mitchellh/goamz/aws"
-	"net/url"
 	"encoding/xml"
-	"strconv"
-	awsauth "github.com/smartystreets/go-aws-auth"
+	"github.com/mitchellh/goamz/aws"
+	"net/http"
+	"net/url"
 	"bytes"
 	"fmt"
+	awsauth "github.com/smartystreets/go-aws-auth"
+	"strings"
 )
 
 type CloudsearchDocument struct {
 	aws.Auth
 	aws.Region
-	endpoint string
+	endpoint   string
 	httpClient *http.Client
 }
 
@@ -30,9 +30,9 @@ func NewWithClient(auth aws.Auth, region aws.Region, endpoint string, httpClient
 
 // Requests
 type Batch struct {
-	XMLName           xml.Name    `xml:"batch"`
-	Adds              []*BatchAdd    `xml:"add"`
-	Deletes           []BatchDelete `xml:"delete"`
+	XMLName xml.Name      `xml:"batch"`
+	Adds    []*BatchAdd   `xml:"add"`
+	Deletes []BatchDelete `xml:"delete"`
 }
 
 func (b *Batch) Add(id string) *BatchAdd {
@@ -45,17 +45,18 @@ func (b *Batch) Delete(id string) {
 }
 
 type BatchDelete struct {
-	Id             string  `xml:"id,attr"`
+	Id string `xml:"id,attr"`
 }
 type BatchAdd struct {
-	Id               string           `xml:"id,attr"`
-	Fields           []BatchAddField  `xml:"field"`
+	Id     string          `xml:"id,attr"`
+	Fields []BatchAddField `xml:"field"`
 }
 
 func (ba *BatchAdd) AddField(name string, value string) {
 	field := &BatchAddField{name, value}
 	ba.Fields = append(ba.Fields, *field)
 }
+
 // Yes, there can be multiple fields with the same name
 func (ba *BatchAdd) GetFields(name string) []BatchAddField {
 	res := []BatchAddField{}
@@ -68,66 +69,51 @@ func (ba *BatchAdd) GetFields(name string) []BatchAddField {
 }
 
 type BatchAddField struct {
-	Name           string    `xml:"name,attr"`
-	Value          string    `xml:",innerxml"`
+	Name  string `xml:"name,attr"`
+	Value string `xml:",innerxml"`
 }
 
 type BatchResult struct {
-	Status            string `xml:"status,attr"`
-	Adds              int    `xml:"adds,attr"`
-	Deletes           int    `xml:"deletes,attr"`
+	Status   string   `xml:"status,attr"`
+	Adds     int      `xml:"adds,attr"`
+	Deletes  int      `xml:"deletes,attr"`
+	Errors   []string `xml:"errors>error"`
+	Warnings []string `xml:"warnings>warning"`
 }
 
-type Error struct {
-	// HTTP status code of the error.
-	StatusCode int
-
-	// AWS code of the error.
-	Code string
-
-	// Message explaining the error.
-	Message string
-}
-
-func (e *Error) Error() string {
-	var prefix string
-	if e.Code != "" {
-		prefix = e.Code+": "
-	}
-	if prefix == "" && e.StatusCode > 0 {
-		prefix = strconv.Itoa(e.StatusCode)+": "
-	}
-	return prefix + e.Message
-}
-
-type xmlErrors struct {
-	Errors []Error `xml:"Error"`
+func (b *BatchResult) Error() string {
+	return strings.Join(b.Errors, "\n")
 }
 
 func (r *CloudsearchDocument) SubmitBatch(batch Batch) (*BatchResult, error) {
-
 	res := &BatchResult{}
 	err := r.query("POST", fmt.Sprintf("/%v/documents/batch", apiVersion), batch, res)
-	switch {
-	case err != nil: return nil, err
-	default: return res, nil
+	if err != nil {
+		return nil, err
 	}
-
+	if res.Status == "error" {
+		return nil, res
+	}
+	return res, nil
 }
 
 func (r *CloudsearchDocument) query(method string, path string, request interface{}, resp interface{}) error {
 	endpoint, err := url.Parse(r.endpoint)
 	endpoint.Path = path
-	xmlBytes, _ := xml.Marshal(request)
+	xmlBytes, err := xml.Marshal(request)
+	if err != nil {
+		return err
+	}
 	xmlReader := bytes.NewReader(xmlBytes)
 	hReq, err := http.NewRequest(method, endpoint.String(), xmlReader)
 	hReq.Header.Set("Content-Type", "application/xml")
+	hReq.Close = true
 	if err != nil {
 		return err
 	}
 
 	awsauth.Sign(hReq, awsauth.Credentials{
-		AccessKeyID: r.Auth.AccessKey,
+		AccessKeyID:     r.Auth.AccessKey,
 		SecretAccessKey: r.Auth.SecretKey,
 	})
 
@@ -138,25 +124,7 @@ func (r *CloudsearchDocument) query(method string, path string, request interfac
 	defer re.Body.Close()
 
 	if re.StatusCode > 200 {
-		return buildError(re)
+		return fmt.Errorf("Cloudsearch returned unexpected http %v", re.Status)
 	}
-	decoder := xml.NewDecoder(re.Body)
-	decodedBody := decoder.Decode(resp)
-	return decodedBody
-}
-
-func buildError(r *http.Response) error {
-	var (
-		err    Error
-		errors xmlErrors
-	)
-	xml.NewDecoder(r.Body).Decode(&errors)
-	if len(errors.Errors) > 0 {
-		err = errors.Errors[0]
-	}
-	err.StatusCode = r.StatusCode
-	if err.Message == "" {
-		err.Message = r.Status
-	}
-	return &err
+	return xml.NewDecoder(re.Body).Decode(resp)
 }
--- cloudsearch/document/document_test.go
+++ cloudsearch/document/document_test.go
@@ -1,10 +1,12 @@
 package document_test
 
 import (
+	"github.com/mitchellh/goamz/aws"
 	"github.com/mitchellh/goamz/cloudsearch/document"
+	"github.com/mitchellh/goamz/testutil"
 	. "github.com/motain/gocheck"
+	"io/ioutil"
 	"testing"
-	"encoding/xml"
 )
 
 func Test(t *testing.T) {
@@ -17,19 +19,63 @@ type S struct {
 
 var _ = Suite(&S{})
 
-func (s *S) TestBatchSerialization(c *C) {
-	batch := &document.Batch{}
+var testServer = testutil.NewHTTPServer()
+
+func (s *S) SetUpSuite(c *C) {
+	testServer.Start()
+	auth := aws.Auth{"abc", "123", ""}
+
+	s.document = document.NewWithClient(auth, aws.USEast, testServer.URL, testutil.DefaultClient)
+}
+
+func (s *S) TearDownTest(c *C) {
+	testServer.Flush()
+}
+
+func (s *S) TestSubmitBatch(c *C) {
+	testServer.Response(200, nil, SubmitBatchSuccessResponse)
+	batch := document.Batch{}
 	add := batch.Add("test")
 	add.AddField("Foo", "Bar")
 	batch.Delete("gone")
 
-	c.Assert(*batch, DeepEquals, document.Batch{Adds: []*document.BatchAdd{&document.BatchAdd{Id: "test", Fields: []document.BatchAddField{document.BatchAddField{"Foo", "Bar"}}}}, Deletes: []document.BatchDelete{document.BatchDelete{"gone"}}})
+	batchResult, err := s.document.SubmitBatch(batch)
+	req := testServer.WaitRequest()
+	c.Assert(err, IsNil)
+	c.Assert(batchResult, NotNil)
+	c.Assert(batchResult.Adds, Equals, 1)
+	c.Assert(batchResult.Deletes, Equals, 1)
 
-	x, _ := xml.MarshalIndent(batch, " ", " ")
-	println(string(x))
-	//	c.Assert(req.Form["Action"], DeepEquals, []string{"CreateDBSecurityGroup"})
-	//	c.Assert(req.Form["DBSecurityGroupName"], DeepEquals, []string{"foobarbaz"})
-	//	c.Assert(req.Form["DBSecurityGroupDescription"], DeepEquals, []string{"test description"})
-	//	c.Assert(err, IsNil)
-	//	c.Assert(resp.RequestId, Equals, "e68ef6fa-afc1-11c3-845a-476777009d19")
+	c.Assert(req.Method, Equals, "POST")
+	c.Assert(req.URL.Path, Equals, "/2013-01-01/documents/batch")
+
+	data, err := ioutil.ReadAll(req.Body)
+	req.Body.Close()
+	c.Assert(err, IsNil)
+	c.Assert(string(data), Equals, `<batch><add id="test"><field name="Foo">Bar</field></add><delete id="gone"></delete></batch>`)
+
+}
+
+func (s *S) TestSubmitBatchReturnsError(c *C) {
+	testServer.Response(200, nil, SubmitBatchErrorResponse)
+	batch := document.Batch{}
+	add := batch.Add("test")
+	add.AddField("Foo", "Bar")
+	batch.Delete("gone")
+
+	batchResult, err := s.document.SubmitBatch(batch)
+	testServer.WaitRequest()
+	c.Assert(err, NotNil)
+	c.Assert(batchResult, IsNil)
+	c.Assert(err.Error(), Equals, "Something went wrong")
+
+}
+
+func (s *S) TestBatchApi(c *C) {
+	batch := document.Batch{}
+	add := batch.Add("test")
+	add.AddField("Foo", "Bar")
+	batch.Delete("gone")
+
+	c.Assert(batch, DeepEquals, document.Batch{Adds: []*document.BatchAdd{&document.BatchAdd{Id: "test", Fields: []document.BatchAddField{document.BatchAddField{"Foo", "Bar"}}}}, Deletes: []document.BatchDelete{document.BatchDelete{"gone"}}})
 }
